9c7da28a1ac3042dab0c33e86eb5c5a3b0ea54a3
thiagoocordo <thiagoocordo@gmail.com>
2023-08-31 21:41:06 -0300

    Proveedor

--- src/Components/Proveedor/ABMProveedor.jsx
+++ src/Components/Proveedor/ABMProveedor.jsx
@@ -4,50 +4,95 @@ import { getDownloadURL, ref, updateMetadata, uploadBytes, uploadBytesResumable 
 import { addDoc, collection, doc, getFirestore, updateDoc } from 'firebase/firestore'
 import { useNavigate } from 'react-router-dom'
 
-const ABMProveedor = () => {
+const ABMProveedor = (detailData) => {
     const [proveedor, setProveedor] = useState({})
-    const handleChange = (event => {
-        let { name, value, type} = event.target
-        setProveedor((proveedor) => {
-            return { ...proveedor, [name]: value }
-          })
 
-    })
-    const navigate = useNavigate();
     const db = getFirestore(app);
+    const navigate = useNavigate();
+    let empty={
+      value:0
+    }
+    const handleChange = (event => {
+      let { name, value, type} = event.target
+      if(value == ""){
+       empty={
+        value:1,
+        nombre:name}
+      }
+      else{
+        empty={
+          value:0
+        }
+        setProveedor((proveedor) => {
+          return { ...proveedor, [name]: value }
+        })
+      }
+    })
+
+    const editDoc = () => {
+      if (empty.value == 0){
+      const examcollref = doc(db, 'proveedores', detailData.proveedor.id)
+      updateDoc(examcollref, proveedor).then(() => {
+        alert("Updated")
+        navigate(0)
+      }).catch(error => {
+        console.log(error.message)
+      })
+      }
+      else{
+        alert("Completar datos de "+empty.nombre)
+      }
+    }
+
     const createDoc = () => {
-        console.log("HOla")
+        let b={
+          value:0,
+        }
+        const inputs = document.querySelectorAll('input');
+        inputs.forEach(input => {
+          if(input.value == ""){
+            b={
+              value:1,
+              nombre:input.name
+            }
+          }
+        })
+        if(b.value == 0){
         const prov={...proveedor,Activo:true}
         const dbRef = collection(db, "proveedores");
         addDoc(dbRef, prov).then((savedDoc) => {
         console.log("Document has been added successfully")
-        alert(`Documento creado: ${savedDoc.id}`)
+        alert(`Documento creado`)
         navigate(0)
         updateDoc(doc(db, 'proveedores', savedDoc.id), { id: savedDoc.id });
         })
         .catch(error => {
             console.log(error)
         })
+      }
+      else{
+        alert("Completar datos de "+b.nombre)
+      }
     }
 
   return (
     <div className='d-flex flex-column align-items-center'>
-         <h2 className="m-auto">Agregar Proveedor</h2>
+         <h2 className="m-auto">{detailData.proveedor.nombre !== ""? "Editar":"Agregar" } Proveedor</h2>
         <div className='d-flex mt-3 gap-5 align-items-center   my-3'>
         <p className='my-0'>Nombre</p>
-        <input type="text" name='nombre' onChange={handleChange} />
+        <input type="text" name='nombre' onChange={handleChange} defaultValue={detailData.proveedor.nombre} required />
         </div>
         <div className='d-flex mt-3 gap-5 align-items-center   my-3'>
         <p className='my-0'>Descripción</p>
-        <input type="text" name='descripcion' onChange={handleChange} />
+        <input type="text" name='descripcion' onChange={handleChange} defaultValue={detailData.proveedor.descripcion} />
         </div>
         <div className='d-flex mt-3 gap-5 align-items-center   my-3'>
         <p className='my-0'>Dirección</p>
-        <input type="text" name='direccion' onChange={handleChange}  />
+        <input type="text" name='direccion' onChange={handleChange} defaultValue={detailData.proveedor.direccion} />
         </div>
         <div className='d-flex gap-4 mt-3 '>
-          <button className='btn btn-success' onClick={createDoc}>Agregar</button>
-          <button className='btn btn-danger'  >Cancelar</button>
+          <button className='btn btn-success' onClick={detailData.proveedor.nombre != ''?editDoc:createDoc}>{detailData.proveedor.nombre != '' ? 'Editar' : 'Agregar'}</button>
+          <button className='btn btn-danger' onClick={() => navigate(0)}>Cancelar</button>
         </div>
     </div>
     
--- src/Components/Proveedor/ProveedoresItem.jsx
+++ src/Components/Proveedor/ProveedoresItem.jsx
@@ -1,31 +1,93 @@
 import React from 'react'
-import { MDBBadge, MDBListGroupItem } from 'mdb-react-ui-kit';
+import { MDBListGroupItem } from 'mdb-react-ui-kit';
 import { useState } from 'react';
 import Popup from 'reactjs-popup';
-import AbmPropiedades from '../AbmPropiedades/AbmPropiedades';
-import { useNavigate } from 'react-router-dom';
-import { BsPencil } from "react-icons/bs";
+import { BsPencil, BsTrash } from "react-icons/bs";
+import { app } from '../../FireBase/config'
+import {doc, getFirestore, updateDoc } from 'firebase/firestore'
+import ABMProveedor from './ABMProveedor';
+import { useNavigate } from 'react-router-dom'
 
 const ProveedoresItem = ({proveedores}) => {
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 5;
+
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = currentPage * itemsPerPage;
+    const currentProveedores = proveedores.slice(startIndex, endIndex);
+
+    const handlePreviousPage = () => {
+        setCurrentPage(Math.max(currentPage - 1, 1));
+    };
+
+    const handleNextPage = () => {
+        const totalPages = Math.ceil(propiedades.length / itemsPerPage);
+        setCurrentPage(Math.min(currentPage + 1, totalPages));
+    };
+
+    const navigate = useNavigate();
+    const db = getFirestore(app);
+    const deleteDoc=(id)=>{
+        console.log(id)
+        const examcollref = doc(db, 'proveedores',id)
+        updateDoc(examcollref, {Activo:false}).then(() => {
+          alert("Deleted")
+          navigate(0)
+        }).catch(error => {
+          console.log(error.message)
+        })
+      }
+
   return (
     <>
     {
-        proveedores.map(({ nombre , descripcion, direccion }) =>
-            <MDBListGroupItem key={nombre} className='d-flex justify-content-between align-items-center' >
+        currentProveedores.map(({id,nombre , descripcion, direccion }) =>
+            <MDBListGroupItem key={id} className='d-flex justify-content-between align-items-center' >
                 <div className='d-flex align-items-center'>
-                    <div className='ms-3'>
-                        <p className='fw-bold mb-1'>{nombre}</p>
-                        <p className='text-muted mb-0'>Actividad: {descripcion}</p>
-                        <p className='text-muted mb-0'>Direccion: {direccion}</p>
+                    <div className="d-flex gap-5">
+                        <div className="ms-3">
+                            <p className='fw-bold mb-1'>Nombre</p>
+                            <p className='text-muted mb-0'>{nombre}</p>
+                        </div>
+                        <div className="ms-3" >
+                            <p className='fw-bold mb-1'>Descripción</p>
+                            <p className='text-muted mb-0'>{descripcion}</p>
+                        </div>
+                        <div className="ms-3">
+                            <p className='fw-bold mb-1'>Direccion</p>
+                            <p className='text-muted mb-0'>{direccion}</p>
+                        </div>
                     </div>
                 </div>
                 <div className='d-flex align-items-center gap-3'>
-                    <Popup trigger={<button  className='btn btn-warning ' onClick={()=>{console.log('Hola')}}><BsPencil></BsPencil></button>} modal>
-                    </Popup>      
+                    <Popup trigger={<button  className='btn btn-warning ' style={{marginRight:'20px'}}><BsPencil ></BsPencil></button>} modal>
+                        <ABMProveedor proveedor={{id,nombre,descripcion,direccion}}></ABMProveedor>
+                    </Popup>
+                    <button className='btn btn-danger' style={{marginRight:'20px'}} onClick={()=>deleteDoc(id)}><BsTrash></BsTrash></button>
                 </div>
             </MDBListGroupItem>
-        )
+        )  
     }
+     <div className="d-flex flex-column align-items-center">
+            <p>
+        <button
+            style={{margin:'5px'}}
+          className="btn btn-secondary"
+          onClick={handlePreviousPage}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+        <button
+            style={{margin:'5px'}}
+          className="btn btn-secondary"
+          onClick={handleNextPage}
+          disabled={endIndex >= proveedores.length}
+        >
+          Next
+        </button>
+        </p>
+      </div>
     </>
   )
 }
