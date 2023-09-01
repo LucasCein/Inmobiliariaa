edf54e7d1bad1a1b110b4b04573e425e672b3984
sanmisanti <sanmisanti@gmail.com>
2023-08-31 23:25:57 -0300

    fix edit

--- src/Components/AbmPropiedades/AbmPropiedades.jsx
+++ src/Components/AbmPropiedades/AbmPropiedades.jsx
@@ -15,18 +15,24 @@ const AbmPropiedades = (detailData) => {
 
   useEffect(() => {
 
-    setPropiedad({nombre: "", estado: "", descripcion:"", imagen:"" , tipo:"", ciudad:"", barrio:"", calle:"", altrua:"", cp:"", piso:"", depto:"", cantBaños:"", cantCuarto:"", wifi:false, aire:false, estacionamiento:false, lavarropa:false, area:"0"})
 
     return () => {
 
 
       if (detailData.propiedades !== '') {
-        console.log(detailData)
+        
         setPropiedad(detailData.propiedad)
+        
+      }
+      else{
+        setPropiedad({id: "",nombre: "", estado: "", descripcion:"", imagen:"" , tipo:"", ciudad:"", barrio:"", calle:"", altura:"", cp:"", piso:"", depto:"", cantBaños:"", cantCuarto:"", wifi:false, aire:false, estacionamiento:false, lavarropa:false, area:"0"})
+
       }
 
     }
   }, [detailData])
+
+  
 
   const optionsStatus = [
     { value: 'ocupado', label: 'Ocupado' },
@@ -78,16 +84,22 @@ const AbmPropiedades = (detailData) => {
   }
   const db = getFirestore(app);
   const createDoc = () => {
-    const prop={...propiedad,visible:true}
-    const dbRef = collection(db, "propiedades");
-    addDoc(dbRef, prop).then((savedDoc) => {
-      console.log("Document has been added successfully")
-      alert(`Documento creado: ${savedDoc.id}`)
-      navigate(0)
-    })
-      .catch(error => {
-        console.log(error)
+    console.log("nombre: ", propiedad.nombre)
+    if (propiedad.nombre != ""){
+      const prop={...propiedad,visible:true}
+      const dbRef = collection(db, "propiedades");
+      addDoc(dbRef, prop).then((savedDoc) => {
+        console.log("Document has been added successfully")
+        alert(`Documento creado: ${savedDoc.id}`)
+        navigate(0)
       })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+    else{
+      alert("Se debe ingresar un nombre de propiedad")
+    }
 
 
   }
@@ -119,19 +131,19 @@ const AbmPropiedades = (detailData) => {
       {/* Renderiza la primera mitad de los elementos aquí */}
       <div className='d-flex mt-3 gap-5 align-items-center   my-3'>
           <p className='my-0'>Nombre</p>
-          <input type="text" name='nombre' value={detailData.propiedad.nombre} onChange={(handleChange)} />
+          <input type="text" name='nombre' value={propiedad.nombre} onChange={(handleChange)} />
         </div>
         <div className='d-flex mt-3 gap-4 align-items-center  my-3'>
           <p className='my-0'>Descripcion</p>
-          <input type="text" name='descripcion' value={detailData.propiedad.descripcion} onChange={handleChange} />
+          <input type="text" name='descripcion' value={propiedad.descripcion} onChange={handleChange} />
         </div>
         <div className='d-flex mt-3 gap-5 align-items-center my-3'>
           <p className='my-0'>Estado</p>
-          <Select className='comboCss basic-single select' options={optionsStatus} defaultValue={(detailData.propiedad.estado != '') ? (detailData.propiedad.estado == 'ocupado') ? optionsStatus[0] : optionsStatus[1] : ""} onChange={(e) => setPropiedad({ ...propiedad, estado: e.value })} name='estado' ></Select>
+          <Select className='comboCss basic-single select' options={optionsStatus} defaultValue={(propiedad.estado != '') ? (propiedad.estado == 'ocupado') ? optionsStatus[0] : optionsStatus[1] : ""} onChange={(e) => setPropiedad({ ...propiedad, estado: e.value })} name='estado' ></Select>
         </div>
         <div className='d-flex mt-3 gap-5 align-items-center  my-3'>
           <p className='my-0'>Tipo</p>
-          <Select className='comboCss2' options={optionsType} defaultValue={(detailData.propiedad.tipo != '') ? (detailData.propiedad.tipo == 'venta') ? optionsType[0] : optionsType[1] : ""} onChange={(e) => setPropiedad({ ...propiedad, tipo: e.value })} ></Select>
+          <Select className='comboCss2' options={optionsType} defaultValue={(propiedad.tipo != '') ? (propiedad.tipo == 'venta') ? optionsType[0] : optionsType[1] : ""} onChange={(e) => setPropiedad({ ...propiedad, tipo: e.value })} ></Select>
         </div>
         <div className='d-flex mt-3 gap-5 align-items-center  my-3 imageAttachment'>
           <p className='my-0'>Imagen</p>
@@ -149,31 +161,31 @@ const AbmPropiedades = (detailData) => {
       {/* Renderiza la primera mitad de los elementos aquí */}
         <div className='d-flex mt-3 gap-4 align-items-center  my-3'>
           <p className=' my-0'>Ciudad</p>
-          <input type="text" value={detailData.propiedad.ciudad} name='ciudad' onChange={handleChange} />
+          <input type="text" value={propiedad.ciudad} name='ciudad' onChange={handleChange} />
         </div>
         <div className='d-flex mt-3 gap-5 align-items-center  my-3'>
           <p className=' my-0'>CP</p>
-          <input type="text" value={detailData.propiedad.cp} name='cp' onChange={handleChange} />
+          <input type="text" value={propiedad.cp} name='cp' onChange={handleChange} />
         </div>
         <div className='d-flex mt-3 gap-4 align-items-center  my-3'>
           <p className=' my-0'>Barrio</p>
-          <input type="text" value={detailData.propiedad.barrio} name='barrio' onChange={handleChange} />
+          <input type="text" value={propiedad.barrio} name='barrio' onChange={handleChange} />
         </div>
         <div className='d-flex mt-3 gap-4 align-items-center  my-3'>
           <p className=' my-0'>Calle</p>
-          <input type="text" value={detailData.propiedad.calle} name='calle' onChange={handleChange} />
+          <input type="text" value={propiedad.calle} name='calle' onChange={handleChange} />
         </div>
         <div className='d-flex mt-3 gap-4 align-items-center  my-3'>
           <p className=' my-0'>Altura</p>
-          <input type="text" value={detailData.propiedad.altura} name='altura' onChange={handleChange} />
+          <input type="text" value={propiedad.altura} name='altura' onChange={handleChange} />
         </div>
         <div className='d-flex mt-3 gap-4 align-items-center  my-3'>
           <p className=' my-0'>Piso</p>
-          <input type="text" value={detailData.propiedad.piso} name='piso' onChange={handleChange} />
+          <input type="text" value={propiedad.piso} name='piso' onChange={handleChange} />
         </div>
         <div className='d-flex mt-3 gap-4 align-items-center  my-3'>
           <p className=' my-0'>Dpto.</p>
-          <input type="text" value={detailData.propiedad.dpto} name='dpto' onChange={handleChange} />
+          <input type="text" value={propiedad.dpto} name='dpto' onChange={handleChange} />
         </div>
         <div className='d-flex flex-row align-items-center'>
           <button className='btn btn-link' onClick={() => handleChangePage(1)} >Atrás</button>
@@ -189,31 +201,31 @@ const AbmPropiedades = (detailData) => {
         <div className='d-flex mt-3 gap-2 align-items-center  my-3'>
           <p className='my-0'>Baños:</p>
           {/* <input type="text" name='cantBaños' value={propiedad.cantBaños} onChange={handleChange} /> */}
-          <Select className='comboCss2' options={roomQty} defaultValue={(detailData.propiedad.cantBaños != 0) ? detailData.propiedad.cantBaños : ""} onChange={(e) => setPropiedad({ ...propiedad, cantBaños: e.value })} ></Select>
+          <Select className='comboCss2' options={roomQty} defaultValue={(propiedad.cantBaños != "") ? roomQty.find(opcion => opcion.label === propiedad.cantBaños) : ""} onChange={(e) => setPropiedad({ ...propiedad, cantBaños: e.value })} ></Select>
         </div>
         <div className='d-flex mt-3 align-items-center  my-3'>
           <p className='my-0'>Cuartos:</p>
-          <Select className='comboCss2' options={roomQty} defaultValue={(detailData.propiedad.cantCuarto != 0) ? detailData.propiedad.cantCuarto : ""} onChange={(e) => setPropiedad({ ...propiedad, cantCuarto: e.value })} ></Select>
+          <Select className='comboCss2' options={roomQty} defaultValue={(propiedad.cantCuarto != "") ? roomQty.find(opcion => opcion.label === propiedad.cantCuarto) : ""} onChange={(e) => setPropiedad({ ...propiedad, cantCuarto: e.value })} ></Select>
         </div>
         <div className='d-flex mt-3 gap-4 align-items-center  my-3'>
           <p className=' my-0'>Area cubierta</p>
-          <input type="text" value={detailData.propiedad.area} name='area' onChange={handleChange} /> m2
+          <input type="text" value={propiedad.area} name='area' onChange={handleChange} /> m2
         </div>
         <div className='d-flex mt-3 gap-4 align-items-center  my-3'>
           <p className='my-0'>WiFi:</p>
-          <input name='wifi' type='checkbox' checked={detailData.propiedad.wifi} onChange={handleChange}/>
+          <input name='wifi' type='checkbox' checked={propiedad.wifi} onChange={handleChange}/>
           <p className='my-0'>Aire Acondicionado:</p>
-          <input name='aire' type='checkbox' checked={detailData.propiedad.aire} onChange={handleChange}/>
+          <input name='aire' type='checkbox' checked={propiedad.aire} onChange={handleChange}/>
           <p className='my-0'>Estacionamiento:</p>
-          <input name='estacionamiento' type='checkbox' checked={detailData.propiedad.estacionamiento} onChange={handleChange}/>
+          <input name='estacionamiento' type='checkbox' checked={propiedad.estacionamiento} onChange={handleChange}/>
           <p className='my-0'>Lavarropas:</p>
-          <input name='lavarropa' type='checkbox' checked={detailData.propiedad.lavarropa} onChange={handleChange}/>
+          <input name='lavarropa' type='checkbox' checked={propiedad.lavarropa} onChange={handleChange}/>
         </div>
         <div className='d-flex flex-column align-items-center'>
           <button className='btn btn-link' onClick={() => handleChangePage(2)} >Atrás</button>
         </div>
         <div className='d-flex gap-4 mt-3 '>
-          <button className='btn btn-success' onClick={detailData.propiedad.nombre != ''?editDoc:createDoc}>{detailData.propiedad.nombre != '' ? 'Editar' : 'Agregar'}</button>
+          <button className='btn btn-success' onClick={detailData.propiedad.nombre != ''? editDoc : createDoc}>{propiedad.nombre != '' ? 'Editar' : 'Agregar'}</button>
           <button className='btn btn-danger' onClick={() => navigate(0)} >Cancelar</button>
           <button className={detailData.propiedad.nombre != ''? 'btn btn-dark':'d-none'} onClick={deleteDoc}>Eliminar</button>
         </div>
