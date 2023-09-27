1d0a13d1727c1f372d6e5724600f6c0e98894faf
nhussein11 <nicolashussein14@gmail.com>
2023-09-27 15:16:30 -0300

    fix: replace duplicated code by using map in side bar

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,64 +1,83 @@
+import {
+  BsCart3,
+  BsGrid1X2Fill,
+  BsFillArchiveFill,
+  BsFillGrid3X3GapFill,
+  BsPeopleFill,
+  BsListCheck,
+  BsMenuButtonWideFill,
+  BsFillGearFill,
+} from "react-icons/bs";
+import { NavLink } from "react-router-dom";
 
-import 
-{BsCart3, BsGrid1X2Fill, BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, 
-  BsListCheck, BsMenuButtonWideFill, BsFillGearFill}
- from 'react-icons/bs'
-import { Link, NavLink } from 'react-router-dom'
+function Sidebar({ openSidebarToggle, OpenSidebar }) {
+  const sideBarItems = [
+    {
+      name: "Dashboard",
+      icon: <BsGrid1X2Fill className="icon" />,
+      path: "/home",
+    },
+    {
+      name: "Propiedades",
+      icon: <BsFillArchiveFill className="icon" />,
+      path: "/properties",
+    },
+    {
+      name: "Proveedores",
+      icon: <BsFillGrid3X3GapFill className="icon" />,
+      path: "/proveedores",
+    },
+    {
+      name: "Pagos",
+      icon: <BsFillGrid3X3GapFill className="icon" />,
+      path: "/pagos",
+    },
+    {
+      name: "Facturas",
+      icon: <BsPeopleFill className="icon" />,
+      path: "/bill",
+    },
+    {
+      name: "Inventory",
+      icon: <BsListCheck className="icon" />,
+      path: "",
+    },
+    {
+      name: "Reports",
+      icon: <BsMenuButtonWideFill className="icon" />,
+      path: "",
+    },
+    {
+      name: "Setting",
+      icon: <BsFillGearFill className="icon" />,
+      path: "",
+    },
+  ];
 
-function Sidebar({openSidebarToggle, OpenSidebar}) {
   return (
-    <aside id="sidebar" className={openSidebarToggle ? "sidebar-responsive": ""}>
-        <div className='sidebar-title'>
-            <div className='sidebar-brand'>
-                <BsCart3  className='icon_header'/> SHOP
-            </div>
-            <span className='icon close_icon' onClick={OpenSidebar}>X</span>
+    <aside
+      id="sidebar"
+      className={openSidebarToggle ? "sidebar-responsive" : ""}
+    >
+      <div className="sidebar-title">
+        <div className="sidebar-brand">
+          <BsCart3 className="icon_header" /> SHOP
         </div>
-
-        <ul className='sidebar-list'>
-            <li className='sidebar-list-item'>
-                <NavLink to="/home">
-                    <BsGrid1X2Fill className='icon'/> Dashboard
-                </NavLink>
-            </li>
-            <li className='sidebar-list-item'>
-                <NavLink to={"/properties"}>
-                    <BsFillArchiveFill className='icon'/> Propiedades
-                </NavLink>
-            </li>
-            <li className='sidebar-list-item'>
-                <NavLink to={"/proveedores"}>
-                    <BsFillGrid3X3GapFill className='icon'/> Proveedores
-                </NavLink>
-            </li>
-            <li className='sidebar-list-item'>
-                <NavLink to={"/pagos"}>
-                    <BsFillGrid3X3GapFill className='icon'/> Pagos
-                </NavLink>
-            </li>
-            <li className='sidebar-list-item'>
-                <NavLink to={"/bill"}>
-                    <BsPeopleFill className='icon'/> Facturas
-                </NavLink>
-            </li>
-            <li className='sidebar-list-item'>
-                <a href="">
-                    <BsListCheck className='icon'/> Inventory
-                </a>
-            </li>
-            <li className='sidebar-list-item'>
-                <a href="">
-                    <BsMenuButtonWideFill className='icon'/> Reports
-                </a>
-            </li>
-            <li className='sidebar-list-item'>
-                <a href="../Properties/Properties.jsx">
-                    <BsFillGearFill className='icon'/> Setting
-                </a>
-            </li>
-        </ul>
+        <span className="icon close_icon" onClick={OpenSidebar}>
+          X
+        </span>
+      </div>
+      <ul className="sidebar-list">
+        {sideBarItems.map((item) => (
+          <li className="sidebar-list-item" key={item.name}>
+            <NavLink to={item.path}>
+              {item.icon} {item.name}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
     </aside>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
