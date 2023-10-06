4be1e604aa2d257033c7af8f7c5318c144ba2aaa
sanmisanti <sanmisanti@gmail.com>
2023-10-05 21:59:11 -0300

    arrgelo de propiedades, falta el filtro de estado

--- src/Components/Properties/Properties.jsx
+++ src/Components/Properties/Properties.jsx
@@ -14,8 +14,9 @@ import "reactjs-popup/dist/index.css";
 import AbmPropiedades from "../AbmPropiedades/AbmPropiedades";
 import Form from "react-bootstrap/Form";
 import ListaDeFiltro from "../listaDeFiltro/ListaDeFiltro";
+import Select from "react-select";
 
-const Properties = ({forSelect,setNomProp,setModalProp,close}) => {
+const Properties = ({ forSelect, setNomProp, setModalProp, close }) => {
   const [propiedades, setPropiedades] = useState([]);
   const [propiedadesToShow, setPropiedadesToShow] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -23,7 +24,13 @@ const Properties = ({forSelect,setNomProp,setModalProp,close}) => {
   const [checkDisponible, setCheckDisponible] = useState(false);
   const [checkVenta, setCheckVenta] = useState(false);
   const [checkEstacionamiento, setCheckEstacionamiento] = useState(false);
-  console.log(forSelect)
+
+  const optionsStatus = [
+    { value: "ocupado", label: "Ocupado" },
+    { value: "disponible", label: "Disponible" },
+  ];
+
+  console.log(forSelect);
   useEffect(() => {
     const dbFirestore = getFirestore();
     const queryCollection = collection(dbFirestore, "propiedades");
@@ -46,7 +53,304 @@ const Properties = ({forSelect,setNomProp,setModalProp,close}) => {
       .finally(() => setIsLoading(false));
   }, []);
 
+  /* return (
+    <section>
+      {isLoading ? (
+        <CustomSpinner></CustomSpinner>
+      ) : (
+        <>
+          <div className="mx-auto w-100 m-1 p-3" style={{ marginTop: "20px" }}>
+            <h1
+              className={forSelect != "" ? "mb-4 text-dark" : "mb-4 text-white"}
+            >
+              Propiedades
+            </h1>
+            <div className="row align-items-center justify-content-center">
+              <div className="col d-flex align-items-center justify-content-start">
+                <div className="mb-3">
+                  <label
+                    className={
+                      forSelect != "" ? "me-2 text-dark" : "me-2 text-white"
+                    }
+                  >
+                    Nombre:
+                  </label>
+                  <input
+                    type="text"
+                    name="nombre"
+                    onChange={(e) => setsearchTituloDesc(e.target.value)}
+                    placeholder="Filtrar"
+                  />
+                </div>
+              </div>
+              <div className="d-flex align-items-center justify-content-start">
+                <label
+                  className={
+                    forSelect != "" ? "me-2 text-dark" : "me-2 text-white"
+                  }
+                >
+                  Estado:
+                </label>
+                <input
+                  className="ms-4"
+                  type="text"
+                  name="CUIT"
+                  onChange={handleChange}
+                  placeholder="Filtrar"
+                />
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </section>
+  ); */
+
+  /* return (
+    <section>
+      {isLoading ? (
+        <CustomSpinner></CustomSpinner>
+      ) : (
+        <>
+          <div className="mx-auto w-100 m-1 p-3" style={{ marginTop: "20px" }}>
+            <h1
+              className={forSelect != "" ? "mb-4 text-dark" : "mb-4 text-white"}
+            >
+              Propiedades
+            </h1>
+            <div className="row align-items-center justify-content-center">
+              <div className="col d-flex align-items-center justify-content-start">
+                <div className="mb-3">
+                  <label
+                    className={
+                      forSelect != "" ? "me-2 text-dark" : "me-2 text-white"
+                    }
+                  >
+                    Nombre:
+                  </label>
+                  <input
+                    type="text"
+                    name="nombre"
+                    onChange={handleChange}
+                    placeholder="Filtrar"
+                  />
+                </div>
+                <div className="mb-3 ms-4">
+                  <label
+                    className={
+                      forSelect != "" ? "me-2 text-dark" : "me-2 text-white"
+                    }
+                  >
+                    Descripción:
+                  </label>
+                  <input
+                    type="text"
+                    name="descripcion"
+                    onChange={handleChange}
+                    placeholder="Filtrar"
+                  />
+                </div>
+              </div>
+              <div className="d-flex align-items-center justify-content-start">
+                <label
+                  className={
+                    forSelect != "" ? "me-2 text-dark" : "me-2 text-white"
+                  }
+                >
+                  CUIT:
+                </label>
+                <input
+                  className="ms-4"
+                  type="text"
+                  name="CUIT"
+                  onChange={handleChange}
+                  placeholder="Filtrar"
+                />
+              </div>
+            </div>
+            <div className=" my-3 d-flex justify-content-end">
+              <Popup
+                trigger={
+                  <button type="button" className="btn btn-success">
+                    Add New
+                  </button>
+                }
+                modal
+              >
+                <ABMProveedor
+                  proveedor={{ nombre: "", descripcion: "", direccion: "" }}
+                ></ABMProveedor>
+              </Popup>
+            </div>
+            <MDBListGroup className="w-100">
+              <div
+                className="container align-items-center justify-content-center pt-1 rounded"
+                style={{ backgroundColor: "black" }}
+              >
+                <div className="row">
+                  <div className="col ms-2">
+                    <p className="fw-bold  text-light">Nombre</p>
+                  </div>
+                  <div className="col ps-3">
+                    <p className="fw-bold text-light">Descripcion</p>
+                  </div>
+                  <div className="col ps-5">
+                    <p className="fw-bold  text-light">CUIT</p>
+                  </div>
+                  <div className="col ps-4">
+                    <p className="fw-bold text-white">E-Mail</p>
+                  </div>
+                  <div className="col ps-5 ms-3">
+                    <p className="fw-bold text-white ps-1">Telefono</p>
+                  </div>
+                  <div className="col d-flex align-items-center justify-content-center me-3">
+                    <p className="fw-bold text-light">Opciones</p>
+                  </div>
+                </div>
+              </div>
+              <ProveedoresItem
+                proveedores={proveedores}
+                forSelected={forSelect}
+                setNomProv={setNomProv}
+                setOpenModal={setOpenModal}
+                close={close}
+              />
+            </MDBListGroup>
+          </div>
+        </>
+      )}
+    </section>
+  ); */
+
   return (
+    <section>
+      {isLoading ? (
+        <CustomSpinner></CustomSpinner>
+      ) : (
+        <>
+          <div className="mx-auto w-100 m-1 p-3" style={{ marginTop: "20px" }}>
+            <div className="mx-auto w-75 ">
+              <div className="w-100 h-100">
+                <div className="d-flex flex-row">
+                  <label className="text-white me-4 row justify-content-center align-items-center">
+                    Filtrar
+                  </label>
+                  <Form.Control
+                    size="lg"
+                    className="mb-3"
+                    type="text"
+                    value={searchTituloDesc}
+                    id="buscador"
+                    onChange={(e) => setsearchTituloDesc(e.target.value)}
+                  />
+                </div>
+                {/* <ListaDeFiltro /> */}
+                <div className="d-flex justify-content-around mb-4">
+                  <Form.Check // prettier-ignore
+                    type="switch"
+                    id="disponibleSwitch"
+                    label="Disponible"
+                    className={forSelect != "" ? "text-dark" : "text-white"}
+                    checked={checkDisponible}
+                    onChange={(e) => setCheckDisponible(e.target.checked)}
+                  />
+                  <Form.Check // prettier-ignore
+                    type="switch"
+                    id="ventaSwitch"
+                    label="Venta"
+                    className={forSelect != "" ? "text-dark" : "text-white"}
+                    checked={checkVenta}
+                    onChange={(e) => setCheckVenta(e.target.checked)}
+                  />
+                  <Form.Check // prettier-ignore
+                    type="switch"
+                    id="estacionamientoSwitch"
+                    label="Estacionamiento"
+                    className={forSelect != "" ? "text-dark" : "text-white"}
+                    checked={checkEstacionamiento}
+                    onChange={(e) => setCheckEstacionamiento(e.target.checked)}
+                  />
+                </div>
+                <div className="d-flex flex-row">
+                  <label className="text-white me-4 row justify-content-center align-items-center">
+                    Estado
+                  </label>
+                  <Select options={optionsStatus}></Select>
+                </div>
+              </div>
+            </div>
+            <div className=" my-3 d-flex justify-content-end">
+              <Popup
+                trigger={
+                  <button type="button" className="btn btn-success">
+                    Add New
+                  </button>
+                }
+                modal
+              >
+                <AbmPropiedades
+                  proveedor={{ nombre: "", descripcion: "", direccion: "" }}
+                ></AbmPropiedades>
+              </Popup>
+            </div>
+            <MDBListGroup className="w-100">
+              <div
+                className="container align-items-center justify-content-center pt-1 rounded"
+                style={{ backgroundColor: "black" }}
+              >
+                <div className="row">
+                  <div className="col ms-5">
+                    <p className="fw-bold  text-light">Imagen</p>
+                  </div>
+                  <div className="col ms-2">
+                    <p className="fw-bold  text-light">Nombre</p>
+                  </div>
+                  <div className="col ps-3">
+                    <p className="fw-bold text-light">Descripcion</p>
+                  </div>
+                  <div className="col ps-2">
+                    <p className="fw-bold  text-light">Estado</p>
+                  </div>
+                  <div className="col d-flex align-items-center justify-content-center me-3">
+                    <p className="fw-bold text-light">Opciones</p>
+                  </div>
+                </div>
+              </div>
+              <PropertiesItems
+                close={close}
+                setModalProp={setModalProp}
+                setNomProp={setNomProp}
+                forSelect={forSelect}
+                propiedades={propiedades.filter(
+                  (propiedad) =>
+                    (propiedad.nombre
+                      .toLowerCase()
+                      .includes(searchTituloDesc.toLowerCase()) ||
+                      propiedad.descripcion
+                        .toLowerCase()
+                        .includes(searchTituloDesc.toLowerCase())) &&
+                    (checkDisponible == true
+                      ? propiedad.estado == "disponible"
+                      : propiedad.estado == "disponible" ||
+                        propiedad.estado == "ocupado") &&
+                    (checkVenta == true
+                      ? propiedad.tipo == "venta"
+                      : propiedad.tipo == "venta" ||
+                        propiedad.tipo == "alquiler") &&
+                    (checkEstacionamiento == true
+                      ? propiedad.estacionamiento == true
+                      : propiedad.estacionamiento == true ||
+                        propiedad.estacionamiento == false)
+                )}
+              />
+            </MDBListGroup>
+          </div>
+        </>
+      )}
+    </section>
+  );
+
+  /* return (
     <section>
       {isLoading ? (
         <CustomSpinner></CustomSpinner>
@@ -64,7 +368,7 @@ const Properties = ({forSelect,setNomProp,setModalProp,close}) => {
                 placeholder="Filtrar"
                 onChange={(e) => setsearchTituloDesc(e.target.value)}
               />
-              {/* <ListaDeFiltro /> */}
+              {/* <ListaDeFiltro /> }
               <div className="d-flex justify-content-around mb-4">
                 <Form.Check // prettier-ignore
                   type="switch"
@@ -162,7 +466,7 @@ const Properties = ({forSelect,setNomProp,setModalProp,close}) => {
         </>
       )}
     </section>
-  );
+  ); */
 };
 
 export default Properties;
--- src/Components/PropertiesItems/PropertiesItems.jsx
+++ src/Components/PropertiesItems/PropertiesItems.jsx
@@ -4,14 +4,26 @@ import Popup from "reactjs-popup";
 import AbmPropiedades from "../AbmPropiedades/AbmPropiedades";
 import { useNavigate } from "react-router-dom";
 import { BsPencil } from "react-icons/bs";
+import { doc, getFirestore, updateDoc } from "firebase/firestore";
+import { app } from "../../FireBase/config";
+import { BsTrash } from "react-icons/bs";
 
-const PropertiesItems = ({ propiedades, forSelect,setNomProp,setModalProp,close }) => {
+const PropertiesItems = ({
+  propiedades,
+  forSelect,
+  setNomProp,
+  setModalProp,
+  close,
+}) => {
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 5;
 
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = currentPage * itemsPerPage;
   const currentItems = propiedades.slice(startIndex, endIndex);
+
+  const navigate = useNavigate();
+  const db = getFirestore(app);
 
   const handlePreviousPage = () => {
     setCurrentPage(Math.max(currentPage - 1, 1));
@@ -22,122 +34,208 @@ const PropertiesItems = ({ propiedades, forSelect,setNomProp,setModalProp,close 
     setCurrentPage(Math.min(currentPage + 1, totalPages));
   };
 
+  /*   const deleteDoc=(id)=>{
+        console.log(id)
+        const examcollref = doc(db, 'propiedades',id)
+        updateDoc(examcollref, {Activo:false}).then(() => {
+          alert("Deleted")
+          navigate(0)
+        }).catch(error => {
+          console.log(error.message)
+        })
+      } */
+
   return (
-<>
-  {currentItems.map(
-    ({
-      id,
-      nombre,
-      descripcion,
-      estado,
-      tipo,
-      pais,
-      region,
-      cp,
-      calle,
-      altura,
-      piso,
-      dpto,
-      cantBaños,
-      cantCuarto,
-      area,
-      wifi,
-      aire,
-      estacionamiento,
-      lavarropa,
-      imagen,
-    }) => (
-      <MDBListGroupItem
-        key={id}
-        className="d-flex justify-content-between align-items-center"
-      >
-        <div className="d-flex align-items-center">
-          <img
-            src={imagen}
-            alt={imagen}
-            style={{ width: "80px", height: "80px", marginLeft: "10px" }}
-            className="rounded-circle"
-          />
-          <div className="ms-3">
-            <p className="fw-bold mb-1">{nombre}</p>
-            <p className="text-muted mb-0">{descripcion}</p>
-          </div>
-        </div>
-        {forSelect == "" ? <div className="d-flex align-items-center gap-3">
-          <Popup
-            className="w-100"
-            trigger={
-              <button className="btn btn-warning ">
-                <BsPencil></BsPencil>
-              </button>
-            }
-            modal
+    <>
+      {currentItems.map(
+        ({
+          id,
+          nombre,
+          descripcion,
+          estado,
+          tipo,
+          pais,
+          region,
+          cp,
+          calle,
+          altura,
+          piso,
+          dpto,
+          cantBaños,
+          cantCuarto,
+          area,
+          wifi,
+          aire,
+          estacionamiento,
+          lavarropa,
+          imagen,
+        }) => (
+          <MDBListGroupItem
+            key={id}
+            className="container align-items-center justify-content-center"
           >
-            <AbmPropiedades
-              propiedad={{
-                id,
-                nombre,
-                descripcion,
-                estado,
-                tipo,
-                pais,
-                region,
-                cp,
-                calle,
-                altura,
-                piso,
-                dpto,
-                cantBaños,
-                cantCuarto,
-                area,
-                wifi,
-                aire,
-                estacionamiento,
-                lavarropa,
-                imagen,
-              }}
-              status={"edit"}
-            ></AbmPropiedades>
-          </Popup>
+            <div className="row ">
+              <div className="col">
+                <img
+                  src={imagen}
+                  alt={imagen}
+                  style={{ width: "80px", height: "80px", marginLeft: "10px" }}
+                  className="rounded-circle"
+                />
+              </div>
+              <div className="col">
+                <p className="mb-0 text-dark">{nombre}</p>
+              </div>
+              <div className="col">
+                <p className="mb-0 text-dark">{descripcion}</p>
+              </div>
+              <div className="col ms-3">
+                <MDBBadge
+                  pill
+                  light
+                  color={estado == "ocupado" ? "danger" : "success"}
+                  className="me-5"
+                >
+                  {estado}
+                </MDBBadge>
+              </div>
 
-          <MDBBadge
-            pill
-            light
-            color={estado == "ocupado" ? "danger" : "success"}
-            className="me-5"
+              <div className="col d-flex align-items-center">
+                <Popup
+                  trigger={
+                    <button className="btn btn-warning ms-5">
+                      <BsPencil></BsPencil>
+                    </button>
+                  }
+                  modal
+                >
+                  <AbmPropiedades
+                    propiedad={{
+                      id,
+                      nombre,
+                      descripcion,
+                      estado,
+                      tipo,
+                      pais,
+                      region,
+                      cp,
+                      calle,
+                      altura,
+                      piso,
+                      dpto,
+                      cantBaños,
+                      cantCuarto,
+                      area,
+                      wifi,
+                      aire,
+                      estacionamiento,
+                      lavarropa,
+                      imagen,
+                    }}
+                    status={"edit"}
+                  ></AbmPropiedades>
+                </Popup>
+                <button className="btn btn-danger ms-2">
+                  <BsTrash></BsTrash>
+                </button>
+              </div>
+            </div>
+
+            {/* <div className="d-flex flex-row align-items-center gap-5">
+              <img
+                src={imagen}
+                alt={imagen}
+                style={{ width: "80px", height: "80px", marginLeft: "10px" }}
+                className="rounded-circle"
+              />
+              <div className="ms-3 d-flex flex-row gap-3">
+                <p className="fw-bold mb-1">{nombre}</p>
+                <p className="text-muted mb-0">{descripcion}</p>
+              </div>
+            </div>
+            {forSelect == "" ? (
+              <div className="d-flex align-items-center gap-3">
+                <Popup
+                  className="w-100"
+                  trigger={
+                    <button className="btn btn-warning ">
+                      <BsPencil></BsPencil>
+                    </button>
+                  }
+                  modal
+                >
+                  <AbmPropiedades
+                    propiedad={{
+                      id,
+                      nombre,
+                      descripcion,
+                      estado,
+                      tipo,
+                      pais,
+                      region,
+                      cp,
+                      calle,
+                      altura,
+                      piso,
+                      dpto,
+                      cantBaños,
+                      cantCuarto,
+                      area,
+                      wifi,
+                      aire,
+                      estacionamiento,
+                      lavarropa,
+                      imagen,
+                    }}
+                    status={"edit"}
+                  ></AbmPropiedades>
+                </Popup>
+
+                <MDBBadge
+                  pill
+                  light
+                  color={estado == "ocupado" ? "danger" : "success"}
+                  className="me-5"
+                >
+                  {estado}
+                </MDBBadge>
+              </div>
+            ) : (
+              <div className="col d-flex align-items-center justify-content-end me-5">
+                <button
+                  onClick={() => {
+                    setNomProp({ nomProp: nombre, idProp: id }), close();
+                  }}
+                  className="btn btn-success"
+                >
+                  Seleccionar
+                </button>
+              </div> 
+            )}*/}
+          </MDBListGroupItem>
+        )
+      )}
+      <div className="d-flex flex-column align-items-center">
+        <p>
+          <button
+            style={{ margin: "5px" }}
+            className="btn btn-secondary"
+            onClick={handlePreviousPage}
+            disabled={currentPage === 1}
           >
-            {estado}
-          </MDBBadge>
-        </div> :
-          <div className='col d-flex align-items-center justify-content-end me-5'>
-            <button onClick={() => { setNomProp({ nomProp: nombre, idProp: id }),close() }} className='btn btn-success' >Seleccionar</button>
-          </div>
-        }
-
-      </MDBListGroupItem>
-    )
-  )}
-  <div className="d-flex flex-column align-items-center">
-    <p>
-      <button
-        style={{ margin: "5px" }}
-        className="btn btn-secondary"
-        onClick={handlePreviousPage}
-        disabled={currentPage === 1}
-      >
-        Previous
-      </button>
-      <button
-        style={{ margin: "5px" }}
-        className="btn btn-secondary"
-        onClick={handleNextPage}
-        disabled={endIndex >= propiedades.length}
-      >
-        Next
-      </button>
-    </p>
-  </div>
-</>
+            Previous
+          </button>
+          <button
+            style={{ margin: "5px" }}
+            className="btn btn-secondary"
+            onClick={handleNextPage}
+            disabled={endIndex >= propiedades.length}
+          >
+            Next
+          </button>
+        </p>
+      </div>
+    </>
   );
 };
 
